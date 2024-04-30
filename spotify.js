f57f8a8b74b0f8a518d2a5e366a93e96a7e03241
AayushGrg333 <aayushgrg333@gmail.com>
2024-04-30 11:41:05 +0545

    all dont album left

--- spotify.js
+++ spotify.js
@@ -1,8 +1,10 @@
 let current_song = new Audio();
-let songs;
+let current_folder;
+let songs; // Declare songs as a global variable
+
 function formatTime(seconds) {
-    if(isNaN(seconds) || seconds < 0){
-        return "00:00"
+    if (isNaN(seconds) || seconds < 0) {
+        return "00:00";
     }
     const minutes = Math.floor(seconds / 60);
     const remainingSeconds = Math.floor(seconds % 60);
@@ -13,40 +15,26 @@ function formatTime(seconds) {
     return `${formattedMinutes}:${formattedSeconds}`;
 }
 
-async function getsongs() {
+async function getsongs(folder) {
+    current_folder = folder;
     let a = await fetch(
-        "http://192.168.18.61:3000/Spotify-clone/assets/songs/"
+        `http://192.168.18.61:3000/Spotify-clone/assets/songs/${folder}/`
     );
     let response = await a.text();
     let div = document.createElement("div");
     div.innerHTML = response;
     let as = div.getElementsByTagName("a");
 
-    let songs = [];
+    songs = [];
     for (let index = 0; index < as.length; index++) {
         const element = as[index];
         if (element.href.endsWith(".m4a")) {
             songs.push(element.href);
         }
     }
-    return songs;
-}
 
-const playmusic = (track, artist) => {
-    current_song.src =
-        "/Spotify-clone/assets/songs/" + track + "-" + artist + ".m4a";
-    current_song.play();
-    const playbutton = document.getElementById("play-pause");
-    playbutton.src = "assets/SVG/pause.svg";
-    document.querySelector(".songinfo").innerHTML = track;
-    document.querySelector(".songtime").innerHTML = "00:00 / 00:00";
-};
-
-async function main() {
-    // get list of all song
-    songs = await getsongs();
     let songul = document.querySelector(".songlibrary ul");
-
+    songul.innerHTML = "";
     for (const song of songs) {
         let decoded = decodeURIComponent(song);
         let raw_song = decoded.split("/").pop();
@@ -64,16 +52,42 @@ async function main() {
                             </li>`;
     }
 
-    // event listener in all songs
-    Array.from(
-        document.querySelector(".songlibrary").getElementsByTagName("li")
-    ).forEach((e) => {
-        e.addEventListener("click", (element) => {
-            let songName = e.querySelector(".info").firstElementChild.innerHTML;
-            let songArtist = e.querySelector("#song-artist").innerHTML;
-            playmusic(songName, songArtist);
-        });
-    });
+    SetupEventListeners(); // No need to pass songs as argument
+}
+
+const playmusic = (url) => {
+    current_song.src = url;
+    current_song.play();
+    document.getElementById("play-pause").src = "assets/SVG/pause.svg";
+    let decoded = decodeURIComponent(url.split("/").pop());
+    let real_song_name = decoded.replace(".m4a", "");
+    let [song_name, song_artist] = real_song_name.split("-");
+    document.querySelector(".songinfo").innerHTML = song_name;
+    document.querySelector(".songtime").innerHTML = "00:00 / 00:00";
+}
+
+async function displayAlbums(){
+    let a = await fetch(
+        `http://192.168.18.61:3000/Spotify-clone/assets/songs/`
+    );
+    let response = await a.text();
+    let div = document.createElement("div");
+    console.log(div)
+    div.innerHTML = response;
+    let anchors = div.getElementsByTagName("a")
+    anchors.forEach(e=>{
+        if(e.href.startsWith("/songs")){
+            console.log(e.href)
+        }
+    })
+    console.log(div)
+}
+
+async function main() {
+    await getsongs("cigarettes");
+
+    // display all the albums on the page
+    displayAlbums()
 
     // attach an event listener to play,pause,next and previous
     const playbutton = document.getElementById("play-pause");
@@ -86,7 +100,6 @@ async function main() {
             playbutton.src = "assets/SVG/playbutton.svg";
         }
     });
-
 
     // Update the position of the seek bar circle and play the song from the clicked position
     document.querySelector(".seekbar").addEventListener("click", (e) => {
@@ -109,48 +122,53 @@ async function main() {
         )}`;
     });
 
-    // add an event listner for hamburder
+    // add an event listener for hamburger
     document.querySelector(".hamburger").addEventListener("click",()=>{
         document.querySelector(".left").style.left = "0"
-    })
+    });
 
     document.querySelector(".close").addEventListener("click",()=>{
         document.querySelector(".left").style.left = "-110%"
-    })
+    });
 
     // add an event listener to previous and next
     let previous_button = document.getElementById("previous-song");
     previous_button.addEventListener("click", () => {
         let index = songs.indexOf(current_song.src);
         let previousindex = (index - 1);
-        if (previousindex >= 0){
-            let previoussongul = songs[previousindex];
-            let decoded = decodeURIComponent(previoussongul);
-            let raw_song = decoded.split("/").pop();
-            let [songName, songArtist] = raw_song.replace(".m4a", "").split("-");
-            playmusic(songName, songArtist);
+        if (previousindex >= 0) {
+            playmusic(songs[previousindex]);
         }
     });
     
     let next_button = document.getElementById("next-song");
     next_button.addEventListener("click", () => {
         let index = songs.indexOf(current_song.src);
-        let nextindex = index + 1;
-        if (nextindex < songs.length){
-            let nextsongul = songs[nextindex];
-            let decoded = decodeURIComponent(nextsongul);
-            let raw_song = decoded.split("/").pop();
-            let [songName, songArtist] = raw_song.replace(".m4a", "").split("-");
-            playmusic(songName, songArtist);
+        let nextindex = (index + 1);
+        if (nextindex < songs.length) {
+            playmusic(songs[nextindex]);
         }
     });
 
     // add an event to volume
     document.querySelector("#volume-range").addEventListener("change", (e) => {
-        console.log("setting volume to",e.target.value, "/100")
-        current_song.volume = parseInt(e.target.value)/100
+        current_song.volume = parseInt(e.target.value) / 100;
     });
-    
 
+    // load the playlist when a card is clicked
+    Array.from(document.getElementsByClassName("card")).forEach(e => {
+        e.addEventListener("click", async item => {
+            await getsongs(`${item.currentTarget.dataset.folder}`);
+        });
+    });
 }
+
+function SetupEventListeners() {
+    Array.from(document.querySelector(".songlibrary ul").children).forEach((li, idx) => {
+        li.addEventListener('click', () => {
+            playmusic(songs[idx]);
+        });
+    });
+}
+
 main();
